0c6905346a9d389ee3683032c98dc50c6c42cd70
Nuker0s <nuker0s2@gmail.com>
2022-12-14 00:26:33 +0100

    ua

---
+++ pliki1/wedkuj.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+    <nav>
+        <div id="gora">
+            <h3>Ryby zamieszkujące rzeki</h3>
+            <ol>
+            </ol>
+        </div>
+        <div id="dol">
+            <h3>Ryby Drapieżne naszych wód</h3>
+            <table>
+                <tr>
+                    <th>L.p</th>
+                    <th>Gatunek</th>
+                    <th>Występowanie</th>
+                </tr>
+        </table>
+        </div>
+    </nav>
+    <div id="prawy">
+        <img src="ryba1.jpg" alt="Sum">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </div>
+    <footer><p>Stronę wykonał:</p></footer>
+</body>
+</html>
