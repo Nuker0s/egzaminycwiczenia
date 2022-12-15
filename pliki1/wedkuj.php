3b96eed016a2c8e76d8314230f5d1c1ae5c87537
Nuker0s <nuker0s2@gmail.com>
2022-12-15 23:59:30 +0100

    w

--- pliki1/wedkuj.php
+++ pliki1/wedkuj.php
@@ -5,33 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="styl_1.css" type="text/css">
+    <link rel="stylesheet" href="styl_1.css" type"text/css">
 </head>
 <body>
     <header>
         <h1>Portal dla wędkarzy</h1>
     </header>
-    <nav>
-        <div id="gora">
-            <h3>Ryby zamieszkujące rzeki</h3>
-            <ol>
-            </ol>
+    <main>
+        <div id="lewo">
+            <div id="gora">
+                <h3>Ryby zamieszkujące rzeki</h3>
+                <ol>
+                </ol>
+            </div>
+            <div id="dol">
+                <h3>Ryby Drapieżne naszych wód</h3>
+                <table>
+                    <tr>
+                        <th>L.p</th>
+                        <th>Gatunek</th>
+                        <th>Występowanie</th>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div id="dol">
-            <h3>Ryby Drapieżne naszych wód</h3>
-            <table>
-                <tr>
-                    <th>L.p</th>
-                    <th>Gatunek</th>
-                    <th>Występowanie</th>
-                </tr>
-        </table>
+        <div id="prawy">
+            <img src="ryba1.jpg" alt="Sum">
+            <a href="kwerendy.txt">Pobierz kwerendy</a>
         </div>
-    </nav>
-    <div id="prawy">
-        <img src="ryba1.jpg" alt="Sum">
-        <a href="kwerendy.txt">Pobierz kwerendy</a>
-    </div>
+        </main>
+    
     <footer><p>Stronę wykonał:</p></footer>
 </body>
 </html>
